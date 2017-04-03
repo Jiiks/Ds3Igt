a513ab8bce9fc6654c39867d973a90c4cdeb621d
Jiiks <jiiks@windowslive.com>
2017-04-03 19:21:42 +0300

    Remove unused imports

--- Ds3Igt/Ds3AutoSplitterSettingsHandler.cs
+++ Ds3Igt/Ds3AutoSplitterSettingsHandler.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace Ds3Igt {
